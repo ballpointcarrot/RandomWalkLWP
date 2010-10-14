7366b783490b7c7f871d5477c7822c526a64cbe0
Christopher Kruse <kruse.christopher@gmail.com>
2010-10-14 00:48:52 -0500

    Issue with force closing fixed. Issue with rendering still there.

--- src/com/cornerofseven/android/lwp/randomwalk/RandomWalk.java
+++ src/com/cornerofseven/android/lwp/randomwalk/RandomWalk.java
@@ -11,74 +11,89 @@ import android.view.SurfaceHolder;
 import java.util.Random;
 
 public class RandomWalk extends Thread{
-    private static final int WAITTIME = 1000/15;
-    private static final int RADIUS = 10;
-    private static final int SHIFT = 24;
-    private SurfaceHolder mHolder;
-    private Random rand = new Random();
-    int width;
-    int height;
-    int xpos;
-    int ypos;
-    Paint dotPaint = new Paint();
-    private int[] colors = {Color.RED, Color.BLUE, Color.GREEN,
-            Color.CYAN, Color.MAGENTA, Color.YELLOW};
+	private static final int WAITTIME = 1000/15;
+	private static final int RADIUS = 10;
+	private static final int SHIFT = 24;
+	private SurfaceHolder mHolder;
+	private Random rand = new Random();
+	int width;
+	int height;
+	int xpos;
+	int ypos;
+	Paint dotPaint = new Paint();
+	private int[] colors = {Color.RED, Color.BLUE, Color.GREEN,
+		Color.CYAN, Color.MAGENTA, Color.YELLOW};
 
-    public RandomWalk(SurfaceHolder holder, Context context){
-        mHolder = holder;
-        Rect size = holder.getSurfaceFrame();
-        setSize(size.width(), size.height());
-        dotPaint.setColor(colors[0]);
-        dotPaint.setStyle(Style.FILL);
-        startWalk();
-    }
+	public RandomWalk(SurfaceHolder holder, Context context){
+		mHolder = holder;
+		Rect size = holder.getSurfaceFrame();
+		setSize(size.width(), size.height());
+		dotPaint.setColor(colors[0]);
+		dotPaint.setStyle(Style.FILL);
+	}
 
-    public void run(){
-        try{
-            Canvas can = mHolder.lockCanvas();
-            can.drawCircle(xpos, ypos, RADIUS, dotPaint);
-            switch(rand.nextInt(4)){
-                case 0: //NORTH
-                    ypos -= SHIFT;
-                    break;
-                case 1: 
-                    xpos += SHIFT;
-                    break;
-                case 2:
-                    ypos += SHIFT;
-                    break;
-                case 3:
-                    xpos -= SHIFT;
-                    break;
-            }
-            dotPaint.setColor(colors[rand.nextInt(6)]);
-            can.drawCircle(xpos,ypos,RADIUS,dotPaint);
-            mHolder.unlockCanvasAndPost(can);
-            wait(WAITTIME);
-        } catch(NullPointerException nex){
-            //doNothing.
-        } catch(InterruptedException iex){
-            //AlsoDoNothing.
-        }
-    }
+	public void run(){
+		try{
+			while(true){
+				Canvas can = mHolder.lockCanvas();
+				can.drawCircle(xpos,ypos,RADIUS,dotPaint);
+				mHolder.unlockCanvasAndPost(can);
+				switch(rand.nextInt(4)){
+					case 0: //NORTH
+						ypos -= SHIFT;
+						break;
+					case 1: 
+						xpos += SHIFT;
+						break;
+					case 2:
+						ypos += SHIFT;
+						break;
+					case 3:
+						xpos -= SHIFT;
+						break;
+				}
+				dotPaint.setColor(colors[rand.nextInt(6)]);
+				can = mHolder.lockCanvas();
+				can.drawCircle(xpos,ypos,RADIUS,dotPaint);
+				mHolder.unlockCanvasAndPost(can);
+				Thread.sleep(WAITTIME);
+				if(ypos < 0){
+					ypos += height;
+				}
+				if(xpos < 0){
+					xpos += width;
+				}
+				if(ypos > height){
+					ypos -= height;
+				}
+				if(xpos > width){
+					xpos -= width;
+				}
+			}
+		} catch(NullPointerException nex){
+			//doNothing.
+		} catch(InterruptedException iex){
+			//AlsoDoNothing.
+		}
+	}
 
-    public void startWalk(){
-        start();
-    }
-    public void pauseWalk(){
-        stopWalk();
-    }
-    public void resumeWalk(){
-        startWalk();
-    }
-    public void stopWalk(){
-    
-    }
+	public void startWalk(){
+		start();
+	}
+	public void pauseWalk(){
+		stopWalk();
+	}
+	public void resumeWalk(){
+		startWalk();
+	}
+	public void stopWalk(){
 
-    public void setSize(int width, int height){
-        this.width = width;
-        this.height = height;
-        xpos = width/2;
-        ypos = height/2;
-    }
+	}
+
+	public void setSize(int width, int height){
+		this.width = width;
+		this.height = height;
+		xpos = width/2;
+		ypos = height/2;
+	}
 }
--- src/com/cornerofseven/android/lwp/randomwalk/RandomWalkService.java
+++ src/com/cornerofseven/android/lwp/randomwalk/RandomWalkService.java
@@ -4,58 +4,57 @@ import android.service.wallpaper.WallpaperService;
 import android.view.SurfaceHolder;
 
 public class RandomWalkService extends WallpaperService{ 
-    private RandomWalkEngine mEngine;
-    public WallpaperService.Engine onCreateEngine(){
-       return mEngine; 
-    }
+	public WallpaperService.Engine onCreateEngine(){
+		return new RandomWalkEngine(); 
+	}
 
-    class RandomWalkEngine extends WallpaperService.Engine{
-        private RandomWalk walker;
-        RandomWalkEngine(){
-            SurfaceHolder holder = getSurfaceHolder();
-            walker = new RandomWalk(holder,getApplicationContext());
-        }
+	class RandomWalkEngine extends WallpaperService.Engine{
+		private RandomWalk walker;
 
-        @Override
-        public void onDestroy(){
-            super.onDestroy();
-            walker.stopWalk();
-        }
+		RandomWalkEngine(){
+			SurfaceHolder holder = getSurfaceHolder();
+			walker = new RandomWalk(holder,getApplicationContext());
+		}
 
-        @Override
-        public void onVisibilityChanged(boolean visible){
-            if(visible){
-                walker.resumeWalk();
-            }
-            else{
-                walker.pauseWalk();
-            }
-        }
-        @Override
-        public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height){
-            super.onSurfaceChanged(holder,format,width,height);
-            walker.setSize(width,height);
-        }
+		@Override
+			public void onDestroy(){
+				super.onDestroy();
+				walker.stopWalk();
+			}
 
-        @Override
-        public void onSurfaceCreated(SurfaceHolder holder){
-            super.onSurfaceCreated(holder);
-            walker.startWalk();
-        }
+		@Override
+			public void onVisibilityChanged(boolean visible){
+				if(visible){
+					walker.resumeWalk();
+				}
+				else{
+					walker.pauseWalk();
+				}
+			}
+		@Override
+			public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height){
+				super.onSurfaceChanged(holder,format,width,height);
+				walker.setSize(width,height);
+			}
 
-        @Override
-        public void onSurfaceDestroyed(SurfaceHolder holder){
-            super.onSurfaceDestroyed(holder);
-            boolean alive = true;
-            walker.stopWalk();
-            while(alive){
-                try{
-                walker.join();
-                alive=false;
-                }
-                catch(InterruptedException iex){
-                }
-            }
-        }
-    }
+		@Override
+			public void onSurfaceCreated(SurfaceHolder holder){
+				super.onSurfaceCreated(holder);
+			}
+
+		@Override
+			public void onSurfaceDestroyed(SurfaceHolder holder){
+				super.onSurfaceDestroyed(holder);
+				boolean alive = true;
+				walker.stopWalk();
+				while(alive){
+					try{
+						walker.join();
+						alive=false;
+					}
+					catch(InterruptedException iex){
+					}
+				}
+			}
+	}
 }
